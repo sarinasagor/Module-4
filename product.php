6f6a7a843e4fea647bf0b3b6c00363d3d09c46b3
Md. Siduzzaman Sagor <56313774+sarinasagor@users.noreply.github.com>
2023-10-04 23:57:51 +0600

    Add files via upload

---
+++ product.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class Product {
+	// Add properties
+	public $id, $name, $price;
+
+	public function __construct( $id, $name, $price ) {
+		//: Initialize properties
+		$this->id    = $id;
+		$this->name  = $name;
+		$this->price = $price;
+	}
+
+
+	//Add getFormattedPrice method
+	public function getFormattedPrice() {
+		return number_format( (float) $this->price, 2, '.', '' );
+	}
+
+	//Add showDetails method
+
+	public function showDetails() {
+		echo "
+        ID: {$this->id}
+        Name: {$this->name}
+        Price: \${$this->getFormattedPrice()}";
+
+	}
+}
+
+
+// Test the Product class
+$product = new Product( 3, 'T-shirtdd', 19.9 );
+$product->showDetails();
